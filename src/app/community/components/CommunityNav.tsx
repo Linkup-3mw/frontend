dc3172f54713b2051b66f820a0fc99d4d1e247cb
moonyah <dltk456@naver.com>
2024-05-18 03:18:22 +0900

    [Chore] add CommunityNav hover

--- src/app/community/components/CommunityNav.tsx
+++ src/app/community/components/CommunityNav.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 export default function CommunityNav() {
+  const [activeIcon, setActiveIcon] = useState('');
+
+  const renderIcon = (iconName: string, iconSource: string, text: string) => {
+    return (
+      <div
+        className="flex flex-col items-center w-[3.5rem] mx-[0.5rem] cursor-pointer"
+        onMouseEnter={() => setActiveIcon(iconName)}
+        onMouseLeave={() => setActiveIcon('')}
+      >
+        <img
+          src={
+            activeIcon === iconName
+              ? `./svg/communityNav/${iconSource}Active.svg`
+              : `./svg/communityNav/${iconSource}.svg`
+          }
+          alt={`${text} Icon`}
+        />
+        <span
+          className={`text-[#8D8D9B] font-bold ${
+            activeIcon === iconName
+              ? 'text-black underline underline-offset-[0.5rem] decoration-2'
+              : ''
+          }`}
+        >
+          {text}
+        </span>
+      </div>
+    );
+  };
+
   return (
     <nav className="h-[3.75rem] bg-blue-100 flex items-center justify-center gap-y-px">
-      <div className="flex flex-col items-center w-[3.5rem] mx-[0.5rem]">
-        <img src="./svg/communityNav/homeIcon.svg" alt="Home Icon" />
-        <span className="text-[#8D8D9B] font-bold">홈</span>
-      </div>
-      <div className="flex flex-col items-center w-[3.5rem] mx-[0.5rem]">
-        <img src="./svg/communityNav/coffeechatIcon.svg" alt="Home Icon" />
-        <span className="text-[#8D8D9B] font-bold">컵챗</span>
-      </div>
-      <div className="flex flex-col items-center w-[3.5rem] mx-[0.5rem]">
-        <img src="./svg/communityNav/clubIcon.svg" alt="Home Icon" />
-        <span className="text-[#8D8D9B] font-bold">소모임</span>
-      </div>
-      <div className="flex flex-col items-center w-[3.5rem] mx-[0.5rem]">
-        <img src="./svg/communityNav/cooperationIcon.svg" alt="Home Icon" />
-        <span className="text-[#8D8D9B] font-bold">협업</span>
-      </div>
-      <div className="flex flex-col items-center w-[3.5rem] mx-[0.5rem]">
-        <img src="./svg/communityNav/eventIcon.svg" alt="Home Icon" />
-        <span className="text-[#8D8D9B] font-bold">이벤트</span>
-      </div>
+      {renderIcon('home', 'homeIcon', '홈')}
+      {renderIcon('coffeechat', 'coffeechatIcon', '컵챗')}
+      {renderIcon('cow', 'cowIcon', '소모임')}
+      {renderIcon('collab', 'collabIcon', '협업')}
+      {renderIcon('event', 'eventIcon', '이벤트')}
     </nav>
   );
 }
