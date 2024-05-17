34ada5fe7c5ed5fe7b40bf902e132f3d76c07064
moonyah <dltk456@naver.com>
2024-05-17 15:39:41 +0900

    [Chore] add HeaderNav

--- src/app/common/header/Header.tsx
+++ src/app/common/header/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import HeaderNav from './HeaderNav';
 
-export default function Header() {
+interface HeaderProps {
+  showNav: boolean;
+}
+
+export default function Header({ showNav }: HeaderProps) {
   return (
-    <div className="bg-blue-200 py-4">
-      <div className="px-[13rem] flex justify-center">
+    <header className="bg-blue-100">
+      <div className="px-[13rem] flex justify-center pt-[1.25rem] pb-[1.19rem]">
         <div className="flex basis-1/3 my-auto text-base">
           <div className="flex justify-center items-center w-[6.375rem] h-[2.5rem] border rounded-full border-black mr-3 font-bold">
             탐색
@@ -15,14 +20,14 @@ export default function Header() {
             공지사항
           </div>
         </div>
-        <div className="basis-1/3 flex justify-center min-w-[12rem]">
-          <img className="my-auto" src="./svg/logo.svg" alt="Logo" />
+        <div className="basis-1/3 flex justify-center min-w-[13rem] mx-3">
+          <img className="my-auto" src="./svg/header/logo.svg" alt="Logo" />
         </div>
         <div className="flex basis-1/3 my-auto justify-end items-center min-w-[25.625rem]">
           <div className="flex mx-3">
             <div className="h-6 w-6 border border-[#45AD56] rounded-full overflow-hidden">
               <img
-                src="./svg/profileDefault.svg"
+                src="./svg/header/profileDefault.svg"
                 alt="Profile Default"
                 className="h-full w-full object-cover"
               />
@@ -30,22 +35,26 @@ export default function Header() {
             <div className="flex mx-2 font-bold">노찬영님</div>
           </div>
           <div className="mx-3">
-            <img src="./svg/chatIcon.svg" alt="Chat Icon" />
+            <img src="./svg/header/chatIcon.svg" alt="Chat Icon" />
           </div>
           <div className="mx-3">
-            <img src="./svg/friendIcon.svg" alt="Friend Icon" />
+            <img src="./svg/header/friendIcon.svg" alt="Friend Icon" />
           </div>
           <div className="mx-3">
             <img
-              src="./svg/unconfirmedAlarmIcon.svg"
+              src="./svg/header/unconfirmedAlarmIcon.svg"
               alt="Unconfirmed Alarm Icon"
             />
           </div>
           <div className="mx-3">
-            <img src="./svg/hamburgerMenuIcon.svg" alt="Hamburger Menu Icon" />
+            <img
+              src="./svg/header/hamburgerMenuIcon.svg"
+              alt="Hamburger Menu Icon"
+            />
           </div>
         </div>
       </div>
-    </div>
+      <div> {showNav && <HeaderNav />} </div>
+    </header>
   );
 }
