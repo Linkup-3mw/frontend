a76f1791b72d684313ba35e5c419cdd3794a6e86
k-wisdom <bnm1354@gmail.com>
2024-05-19 00:16:35 +0900

    [Chore] Change folder organization

--- src/app/(auth)/components/RoundedFrame.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  children: React.ReactNode;
-}
-
-export default function RoundedFrame({ children }: Props) {
-  return <div className="p-10 rounded-3xl bg-blue-50">{children}</div>;
-}
