59815b55859f7e5eef3c8fc972faeb73cc1645eb
k-wisdom <bnm1354@gmail.com>
2024-05-27 22:29:58 +0900

    [Design] ContentWrap component add classname

--- src/app/common/components/frame/ContentWrap.tsx
+++ src/app/common/components/frame/ContentWrap.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
-export default function ContentWrap({ children }: Props) {
+export default function ContentWrap({ children, className }: Props) {
   return (
-    <div className="mx-auto max-w-[95rem] w-full box-border">{children}</div>
+    <div className={`mx-auto max-w-[95rem] w-full box-border ${className}`}>
+      {children}
+    </div>
   );
 }
