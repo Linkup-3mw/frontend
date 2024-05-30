0783e94f1d8af7dde06f3452e697f6a3b8b6c7b1
k-wisdom <bnm1354@gmail.com>
2024-05-30 11:57:26 +0900

    [Fix] Nextauth options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { AuthUser } from '@/types/user';
 import { signInWithCredentials } from '@/app/service/auth';
 
 export const authOptions: NextAuthOptions = {
@@ -9,18 +8,24 @@ export const authOptions: NextAuthOptions = {
       credentials: {
         email: { label: 'Email' },
         password: { label: 'Password', type: 'password' },
+        remember_me: { label: 'remember_me', type: 'checkbox' },
       },
       async authorize(credentials): Promise<any> {
         if (!credentials?.email || !credentials?.password) {
           throw new Error('Invalid credentials');
         }
 
-        const user: AuthUser = await signInWithCredentials(credentials);
+        const params = {
+          email: credentials.email,
+          password: credentials.password,
+          remember_me: credentials.remember_me === 'true' ? true : false,
+        };
 
-        if (user) {
-          return user;
-        } else {
-          return null;
+        try {
+          const res = await signInWithCredentials(params);
+          return res.data.data;
+        } catch (e: any) {
+          throw new Error(e.response.data.message);
         }
       },
     }),
