55ff010cab2d43ded673bf9398a1016250f5952b
k-wisdom <bnm1354@gmail.com>
2024-06-01 12:05:17 +0900

    [Design] Add Club member swiper

---
+++ src/app/common/components/icons/SquareBlueArrow.tsx
@@ -0,0 +1,35 @@
+export const SquareBlueArrowNext = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <path
+        d="M0 4.5C0 2.29086 1.79086 0.5 4 0.5H20C22.2091 0.5 24 2.29086 24 4.5V20.5C24 22.7091 22.2091 24.5 20 24.5H4C1.79086 24.5 0 22.7091 0 20.5V4.5Z"
+        fill="#BFD4FF"
+      />
+      <path d="M8 20.5L16 12.5L8 4.5" stroke="white" strokeWidth="1.5" />
+    </svg>
+  );
+};
+
+export const SquareBlueArrowPrev = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <path
+        d="M0 4.5C0 2.29086 1.79086 0.5 4 0.5H20C22.2091 0.5 24 2.29086 24 4.5V20.5C24 22.7091 22.2091 24.5 20 24.5H4C1.79086 24.5 0 22.7091 0 20.5V4.5Z"
+        fill="#BFD4FF"
+      />
+      <path d="M16 4.5L8 12.5L16 20.5" stroke="white" strokeWidth="1.5" />
+    </svg>
+  );
+};
