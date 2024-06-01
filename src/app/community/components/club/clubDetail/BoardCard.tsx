69967cccb79592864f2943a51d78c22754a6613e
k-wisdom <bnm1354@gmail.com>
2024-06-01 12:07:05 +0900

    [Design] Club member page publishing

---
+++ src/app/community/components/club/clubDetail/BoardCard.tsx
@@ -0,0 +1,52 @@
+import {
+  CommentIcon,
+  SpeakerIcon,
+} from '@/app/common/components/icons/MeetingIcons';
+import Avatar from '@/app/common/components/user/Avatar';
+
+interface Props {
+  isDetail?: boolean;
+}
+export default function BoardCard({ isDetail = false }: Props) {
+  return (
+    <div className="p-[1.5rem] bg-white rounded-[0.5rem]">
+      <h3 className="mb-[1.5rem] text-[1rem] font-bold">
+        {isDetail && (
+          <SpeakerIcon className="inline-block mr-[0.5rem] align-baseline stroke-[1.5px]" />
+        )}
+        소모임 상세 소개 등등 제목
+      </h3>
+      <div className="mb-[2rem] min-h-[1.875rem] leading-[142.85%] whitespace-pre-line two_line_dot_three text-[0.875] max-md:mb-[1.91rem] max-md:text-[0.75rem]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod totam unde
+        accusamus, architecto a atque non, laborum doloribus quisquam corporis
+        iusto, facere labore quos consectetur ratione ipsa quo molestias iure.
+      </div>
+      <div className="flex items-end justify-between">
+        <div className="flex gap-[1rem] items-center">
+          <Avatar
+            image="img_office_1.png"
+            name="김민수"
+            className="border-none w-[2rem] h-[2rem]"
+          />
+          <span className="text-[0px]">
+            <b className="block mb-[0.5] text-[0.875rem] text-gray-800">
+              김민수
+            </b>
+            <i className="text-[0.75rem] text-gray-500 font-bold not-italic">
+              디자인
+            </i>
+          </span>
+        </div>
+        <div className="text-[0.75rem]">
+          {!isDetail && (
+            <span className="inline-block mr-[1rem]">
+              <CommentIcon className="inline-block mr-[0.5rem] stroke-[1.5px] align-middle" />
+              13
+            </span>
+          )}
+          <i className="not-italic text-[#8d8d8d]">2024.05.11</i>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/community/components/club/clubDetail/IntroduceSection.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image';
+import MemberSwiper from './MemberSwiper';
+import { IntroduceTopBtn } from './IntroduceTopBtn';
+import { ClubDetail } from '@/app/community/(pages)/club/[id]/page';
+
+export default function IntroduceSection({
+  club_name,
+  club_location,
+  club_category,
+  detailed_introduction,
+}: ClubDetail) {
+  return (
+    <>
+      <img
+        src="/images/home/img_card_3.png"
+        alt={club_name + '대표 이미지'}
+        className="w-full aspect-[2.07/1] object-cover rounded-[0.5rem] max-md:aspect-[3.2/1] max-md:rounded-none"
+      />
+      <div className="max-md:relative max-md:mt-[1.62rem]">
+        <div className="flex justify-between gap-[1rem]">
+          <h2 className="my-[1.75rem] text-[1.75rem] font-bold leading-none max-md:mt-[1.69rem] max-md:mb-[1.38rem] max-md:text-[1rem]">
+            {club_name}
+          </h2>
+          <div className="flex items-center gap-[0.25rem] max-md:hidden">
+            <IntroduceTopBtn />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-[1.5rem] mb-[1.78rem] max-md:-mt-[0.575rem]  max-md:mb-[1.56rem]">
+          <span className="flex items-center max-md:absolute max-md:top-0 max-md:left-0">
+            <Image
+              width={40}
+              height={40}
+              src="/svg/club/locationIcon.svg"
+              alt="위치"
+              className="mr-[0.5rem] w-[2.5rem] h-[2.5rem] max-md:w-[1rem] max-md:h-[1rem]"
+            />
+            <b className="text-[1.25rem] max-md:text-[0.75rem]">
+              {club_location}
+            </b>
+          </span>
+          <div className="flex gap-4 items-center text-sm mt-2 font-semibold max-md:gap-[0.5rem] max-md:text-[0.75rem]">
+            <span className="bg-yellow-600 p-[0.5rem] rounded leading-none">
+              {club_category}
+            </span>
+            <span className="bg-yellow-600 p-[0.5rem] rounded leading-none">
+              모임 D-23
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-[3.19rem]">
+        <span className="flex flex-shrink-0 gap-4 mt-2 text-sm ">
+          <div className="flex items-center mb-[0.3125rem] text-[1.25rem] font-bold max-md:text-[0.75rem]">
+            <Image
+              src="/svg/club/peoplesIcon.svg"
+              alt="Peoples Icon"
+              className="mr-[0.5rem] w-[2.5rem] h-[2.5rem] max-md:mr-[0.25rem] max-md:w-[1rem] max-md:h-[1rem]"
+              width={40}
+              height={40}
+            />
+            멤버 (20/25)
+          </div>
+        </span>
+        <div>
+          <MemberSwiper
+            swiperOption={{
+              breakpoints: {
+                768: {
+                  grid: { rows: 2, fill: 'row' },
+                },
+              },
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="hidden max-md:mt-[1.5rem] max-md:block max-md:max-h-[13.125rem] max-md:text-[0.875rem]  max-md:leading-[142%]">
+        {detailed_introduction}
+      </div>
+    </>
+  );
+}
