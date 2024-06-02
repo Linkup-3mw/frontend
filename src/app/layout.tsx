308c46c4cb6ec00d51f7a476b0f803d61fc1185a
k-wisdom <bnm1354@gmail.com>
2024-06-02 16:02:19 +0900

    [Feat] ADD Header login, logout button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${suit.className} bg-blue-100`}>
-        <Header />
         <QueryProvider>
           <RecoilProvider>
             <AuthContext>
               <AuthorizationHeader>
+                <Header />
                 <div>{children}</div>
               </AuthorizationHeader>
             </AuthContext>
