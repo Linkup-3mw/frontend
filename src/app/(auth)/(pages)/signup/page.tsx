d2c811166ca6bad25eecf0c8d1a1c9ea88699c44
k-wisdom <bnm1354@gmail.com>
2024-06-03 01:07:50 +0900

    [Feat] Add  Mobile back button

--- src/app/(auth)/(pages)/signup/page.tsx
+++ src/app/(auth)/(pages)/signup/page.tsx
@@ -1,6 +1,7 @@
 import Container from '@/app/(auth)/components/common/Container';
 import RoundedFrame from '@/app/(auth)/components/common/RoundedFrame';
 import SignupForm from '@/app/(auth)/components/signup/SignupForm';
+import MobileBackBtn from '@/app/common/components/form/MobileBackBtn';
 
 interface Props {
   searchParams: {
@@ -13,9 +14,12 @@ export default function SignupPage({ searchParams: { type } }: Props) {
   return (
     <Container>
       <RoundedFrame>
-        <h2 className="mb-[2.5rem] text-center text-[1.5rem] font-bold leading-none max-md:mb-[2.08rem] max-md:text-[1.25rem]">
-          {!type ? '개인 회원가입' : '기업 회원가입'}
-        </h2>
+        <div className="relative">
+          <MobileBackBtn className="absolute left-0 -top-[0.3rem]" />
+          <h2 className="mb-[2.5rem] text-center text-[1.5rem] font-bold leading-none max-md:mb-[2.08rem] max-md:text-[1.25rem]">
+            {!type ? '개인 회원가입' : '기업 회원가입'}
+          </h2>
+        </div>
         <SignupForm type={type} />
       </RoundedFrame>
     </Container>
--- src/app/common/components/form/MobileBackBtn.tsx
+++ src/app/common/components/form/MobileBackBtn.tsx
@@ -1,9 +1,28 @@
 'use client';
 
-export default function MobileBackBtn() {
+import { useRouter } from 'next/navigation';
+
+export default function MobileBackBtn({ className }: { className: string }) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.back();
+  };
   return (
-    <button>
+    <button
+      className={`max-md:block hidden w-[1.5rem] h-[1.5rem] ${className}`}
+      onClick={handleClick}
+    >
       <span className="hide">뒤로가기</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        className="inline-block"
+      >
+        <path d="M16 20L8 12L16 4" stroke="#171717" strokeWidth="1.5" />
+      </svg>
     </button>
   );
 }
