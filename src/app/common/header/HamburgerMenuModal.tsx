4c3e964fc5fcab9076b9ad62b85c12846380ec9c
moonyah <dltk456@naver.com>
2024-06-05 13:19:54 +0900

    [Design] Fix Header mobile publishing

--- src/app/common/header/HamburgerMenuModal.tsx
+++ src/app/common/header/HamburgerMenuModal.tsx
@@ -3,12 +3,12 @@ import { Session } from 'next-auth';
 import Link from 'next/link';
 
 interface HamburgerMenuModalProps {
-  session: Session | null;
-  isOpen: boolean;
+  session?: Session | null;
+  isOpen?: boolean;
   onClose: () => void;
 }
 
-const MenuItems = () => (
+const MenuItems = ({ onClose }: HamburgerMenuModalProps) => (
   <>
     <div className="p-[2rem] border-b border-gray-100">
       <div className="pb-[1.5rem] leading-none text-lg md:text-xl lg:text-2xl">
@@ -16,10 +16,14 @@ const MenuItems = () => (
       </div>
       <ul className="leading-none text-gray-600 text-sm md:text-base lg:text-lg">
         <li className="mb-4">
-          <Link href="/company">회사 소개</Link>
+          <Link href="/company" onClick={onClose}>
+            회사 소개
+          </Link>
         </li>
         <li>
-          <Link href="/membership">멤버십 안내</Link>
+          <Link href="/membership" onClick={onClose}>
+            멤버십 안내
+          </Link>
         </li>
       </ul>
     </div>
@@ -29,7 +33,9 @@ const MenuItems = () => (
       </div>
       <ul className="leading-none text-gray-600 text-sm md:text-base lg:text-lg">
         <li>
-          <Link href="/map">지점 찾기 / 예약하기</Link>
+          <Link href="/map" onClick={onClose}>
+            지점 찾기 / 예약하기
+          </Link>
         </li>
       </ul>
     </div>
@@ -39,7 +45,9 @@ const MenuItems = () => (
       </div>
       <ul className="leading-none text-gray-600 text-sm md:text-base lg:text-lg">
         <li>
-          <Link href="/community/club">소모임</Link>
+          <Link href="/community/club" onClick={onClose}>
+            소모임
+          </Link>
         </li>
       </ul>
     </div>
@@ -49,10 +57,14 @@ const MenuItems = () => (
       </div>
       <ul className="leading-none text-gray-600 text-sm md:text-base lg:text-lg">
         <li className="mb-4">
-          <Link href="/faq">자주 묻는 질문</Link>
+          <Link href="/faq" onClick={onClose}>
+            자주 묻는 질문
+          </Link>
         </li>
         <li>
-          <Link href="/support">1:1 문의하기</Link>
+          <Link href="/support" onClick={onClose}>
+            1:1 문의하기
+          </Link>
         </li>
       </ul>
     </div>
@@ -84,18 +96,23 @@ export default function HamburgerMenuModal({
         ✕
       </div>
       {session ? (
-        <div className="pt-[2rem] pb-[2rem] px-[2.5rem] bg-blue-50 rounded-t-xl">
-          <div className="mb-4 leading-none text-lg md:text-xl lg:text-2xl">
-            {session.user.name}님
+        <>
+          <div className="pt-[2rem] pb-[2rem] px-[2.5rem] bg-blue-50 rounded-t-xl">
+            <div className="mb-4 leading-none text-lg md:text-xl lg:text-2xl">
+              {session.user.name}님
+            </div>
+            <div className="flex gap-[1rem] leading-none text-gray-900 text-sm md:text-base lg:text-lg">
+              <Link href="/profile">내 정보</Link>
+              <button onClick={() => signoutWithCredentials()}>로그아웃</button>
+            </div>
           </div>
-          <div className="flex gap-[1rem] leading-none text-gray-900 text-sm md:text-base lg:text-lg">
-            <Link href="/profile">내 정보</Link>
-            <button onClick={() => signoutWithCredentials()}>로그아웃</button>
+          <div className="overflow-y-auto max-mb:h-[90vh] max-h-[80vh]">
+            <MenuItems onClose={onClose} />
           </div>
-        </div>
+        </>
       ) : (
         <div className="overflow-y-auto max-mb:h-[90vh] max-h-[80vh]">
-          <MenuItems />
+          <MenuItems onClose={onClose} />
         </div>
       )}
     </div>
