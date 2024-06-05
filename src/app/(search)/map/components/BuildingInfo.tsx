f639d12f6f5c73fe5531c19a9215f0c01965df5a
mac <mac@macui-MacBookPro.local>
2024-06-05 16:07:29 -0700

    [Chore] build error 수정

--- src/app/(search)/map/components/BuildingInfo.tsx
+++ src/app/(search)/map/components/BuildingInfo.tsx
@@ -23,11 +23,11 @@ export default function BuildingInfo() {
       }
     };
     if (id) fetchBuildingsData();
-  }, []);
+  }, [BuildingId, currentBuilding, id]);
   return (
     <>
       <div
-        className="hidden-md mb:h-[43.25rem] mb:w-full md:w-[30.6875rem] md:h-[56.75rem]  mb:bottom-0 z-[991]
+        className="hidden-md  mb:h-[43.25rem] mb:w-full md:w-[30.6875rem] md:h-[56.75rem]  mb:bottom-0 z-[991] 
         md:fixed md:top-[110px]  md:rounded-3xl md:shadow-2xl md:right-[530px] md:bottom-5 
         rounded-3xl flex flex-col mb:top-[70px] absolute ease-in duration-300 shadow-2xl mb:right-[0px] bottom-5 bg-white p-0 m-0 
        "
--- src/app/(search)/reservation/myMembership/components/AllSearchMembership.tsx
+++
@@ -1,4 +0,0 @@
-//api/v1/reservation/my-membership
-export default function AllSearchMembership() {
-  return <div>사용자 멤버십 전체 조회 </div>;
-}
