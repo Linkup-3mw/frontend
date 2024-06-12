7b56f8cb06f55dfc9632d926e73b02469612c936
moonyah <dltk456@naver.com>
2024-06-12 10:35:28 +0900

    [Design] Add login home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next';
 import NoneLoginHome from './(home)/components/noneLogin/NoneLoginHome';
+import LoginHome from './(home)/components/login/LoginHome';
+import { getSession } from '@/utils/getSession';
+
 export const metadata: Metadata = {
   title: 'Linkup',
   description: '공간과 사람의 연결을 통해 얻는 새로운 가치',
 };
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
-
-  //임시로 모든 사용자에게 보여줌
-  return <NoneLoginHome />;
+  const session = await getSession();
+  return session ? <LoginHome /> : <NoneLoginHome />;
 }
