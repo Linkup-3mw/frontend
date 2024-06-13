2426169946be24bab96efefc3fd5bcd7b7a18298
mac <mac@macui-MacBookPro.local>
2024-06-13 02:51:13 -0700

    Commit message

--- src/app/(search)/map/components/BuildingFilter.tsx
+++ src/app/(search)/map/components/BuildingFilter.tsx
@@ -125,7 +125,7 @@ export default function BuildingFilter({ isUp }: BuildingFilterProps) {
           <div className="flex items-center">
             <span className="mr-2">지역 선택</span>
             <Image
-              src="/svg/map/Arrow.svg"
+              src="/svg/map/arrow.svg"
               width={24}
               height={24}
               alt="화살표"
@@ -311,7 +311,7 @@ export default function BuildingFilter({ isUp }: BuildingFilterProps) {
           <div className="flex items-center">
             <span className="mr-2">직군</span>
             <Image
-              src="/svg/map/Arrow.svg"
+              src="/svg/map/arrow.svg"
               width={24}
               height={24}
               alt="화살표"
